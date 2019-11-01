8b16af32d0f320b57513e19290e33cf615cec65f
ryan.chen <dobeco@163.com>
2019-11-01 17:54:38 +0800

    删除loading

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
-  theme: 'reco',
+  // theme: 'reco',
   themeConfig: themeConfig,
   markdown: {
     //lineNumbers: true
